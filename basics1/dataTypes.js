2832bec18a954a6faad591b3ffab3de2a94f7f57
nandeenisoni <nandeenisoni3@gmail.com>
2024-10-03 18:32:21 +0000

    new files for commit

---
+++ basics1/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all javascript code as newest version
+
+// alert("Hello")  we afre using node js not browser
+
+console.log(3
+    +3);   // code readilibity should be high
+console.log("Nandeeni")
+
+let name = "Nandeeni"
+let age = 27
+let isLoogedIn = false
+//Primitive Data Type
+//number  =>2 to power of 53
+//bidint 
+// string => ""
+//boolean => true/false
+// null = standalone value where empty space is declared like let name ="";  null is object  typeOf output
+// undefined => DEclared but not assigned any value like let name; typeof output undefined
+// Symbol => to find uniqueness
+
+//NOn primitive
+//Object
---
+++ basics1/variables.js
@@ -0,0 +1,12 @@
+const accountId = 144555
+let accounEmail = "nandeeni@gmail.com"
+var accountPassword = "3892ehde7yd38"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2  not allowed
+accounEmail="kjdsbxsj"
+
+/*
+Prefer not to use var because of issue of block scope and function scope */
+console.table([accountId, accounEmail, accountPassword, accountCity,accountState])
